81f0272488daecdfc13765b60414c061625d95be
Sam Ourn <sourn@mail.greenriver.edu>
2018-01-11 14:09:58 -0500

    Added composer.json

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+//Require the autoload file
+require_once ('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Hello!</h1>';
+}
+);
+
+//Run Fat-Free
+$f3->run();
+?>
